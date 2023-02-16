ee240f47189370d6c6d83d416a2988cb3fd8ae56
kawinkumar25 <123533543+kawinkumar25@users.noreply.github.com>
2023-02-17 00:11:31 +0530

    Update Sqlexeception.java

--- src/main/java/org/example/Sqlexeception.java
+++ src/main/java/org/example/Sqlexeception.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.logging.Logger;
 
-public class Jdbc {
+public class Sqlexeception {
     public static void main(String[] args) {
         String username;
         String password;
