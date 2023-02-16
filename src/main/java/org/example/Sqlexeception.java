a729dedd45db95edf2e028dc2b7f5ced3263d2ab
kawinkumar25 <123533543+kawinkumar25@users.noreply.github.com>
2023-02-17 00:09:20 +0530

    Update Sqlexeception.java

--- src/main/java/org/example/Sqlexeception.java
+++ src/main/java/org/example/Sqlexeception.java
@@ -1,11 +1,11 @@
 package org.example;
 
+import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Scanner;
 import java.util.logging.Logger;
 
-public class Sqlexeception {
+public class Jdbc {
     public static void main(String[] args) {
         String username;
         String password;
@@ -21,16 +21,16 @@ public class Sqlexeception {
         l.info("Enter password:");
         password = inp.next();
 
-        Dbconn ob1 = Dbconn.in(database, username, password);
+        Dbconn dbconnobj = Dbconn.in(database, username, password);
 
         boolean loop = true;
         while (loop) {
             l.info("1.connection estabilished\n2.connection close\n3.exit");
             int ch = inp.nextInt();
             if (ch == 1) {
-                ob1.connection();
+                dbconnobj.connection();
             } else if (ch == 2) {
-                ob1.close();
+                dbconnobj.close();
             } else if (ch == 3) {
                 loop = false;
                 l.info("Exited");
@@ -41,17 +41,20 @@ public class Sqlexeception {
     }
 }
 
-class Dbconn {
+ class Dbconn {
     String username;
     String password;
     String database;
     Connection conn;
-    Logger l = Logger.getLogger("kawin");
+     Logger l = Logger.getLogger("kawin");
     private Dbconn(String database, String username, String password) {
         this.database = database;
         this.username = username;
         this.password = password;
     }
+     //database name: jdbc:mysql://localhost:3306/
+     //password:12345678
+     //username: root
 
     public static Dbconn in(String database, String username, String password) {
         return new Dbconn(database, username, password);
@@ -75,3 +78,4 @@ class Dbconn {
         }
     }
 }
+
