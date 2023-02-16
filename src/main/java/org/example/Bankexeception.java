8810466f93b2c4c29feba7d60ab0437428ae7062
kawinkumar25 <123533543+kawinkumar25@users.noreply.github.com>
2023-02-16 23:42:44 +0530

    Update Bankexeception.java

--- src/main/java/org/example/Bankexeception.java
+++ src/main/java/org/example/Bankexeception.java
@@ -2,84 +2,92 @@ package org.example;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 public class Bankexeception {
 
-    String name;
-    long accNo;
-    double balance=0;
-    Scanner dep =new Scanner(System.in);
-    Scanner i = new Scanner(System.in);
-    Scanner s = new Scanner(System.in);
-    Logger l = Logger.getLogger("kawin");
-    Bankexeception()
+    public static void main(String [] args)
     {
+        Scanner inputobject = new Scanner(System.in );
+        Logger l = Logger.getLogger("kawin");
         l.info("Enter name:");
-        name=i.nextLine();
+        String name=inputobject.nextLine();
+        long accno=0;
         try
         {
             l.info("Enter Account number:");
-            accNo=s.nextLong();
+            accno=inputobject.nextLong();
         }
         catch (InputMismatchException ex) {
-            l.info("Wrong datatype is entered");
             l.info(String.valueOf(ex));
         }
+        Bankinfo inputobj = new Bankinfo(name,accno);
 
+        boolean inc = true;
+
+        while(inc)
+        {
+            l.info(" 1 Deposit \n 2 Withdrawl \n 3 Balance \n 4 Exit");
+            int choice = inputobject.nextInt();
+            switch (choice) {
+                case 1 -> inputobj.deposit();
+                case 2 -> inputobj.withdrawl();
+                case 3 -> inputobj.display();
+                case 4 -> inc = false;
+                default -> l.info("invalid choice");
+            }
+
+        }
+    }
+}
+class Bankinfo
+{
+    String name;
+    long accno;
+    double balance;
+    Scanner bankinputobject =new Scanner(System.in);
+    Logger l = Logger.getLogger("kawin");
+    Bankinfo(String name,long accno)
+    {
+        this.name=name;
+        this.accno=accno;
+        balance=0;
     }
 
     void deposit()
     {
         l.info("Enter Deposit amount:");
-        int deposit = dep.nextInt();
-        balance = balance+deposit;
+        int deposit = bankinputobject.nextInt();
+        this.balance = balance+deposit;
         curBal();
     }
 
     void withdrawl()
     {
         l.info("Enter withdrawl amount:");
-        int withdrawl = dep.nextInt();
+        int withdrawl = bankinputobject.nextInt();
         if(withdrawl<=balance)
         {
-            balance = balance-withdrawl;
+            this.balance = balance-withdrawl;
         }
         else
         {
-            l.log(Level.INFO, ()->"Access denied, current balance is "+ balance + "\nEnter less than balance amount..");
-            withdrawl();
+            l.log(Level.INFO,()-> "Access denied, current balance is "+ balance + "\nEnter less than balance amount..");
+            if(balance>0)
+            {
+                withdrawl();
+            }
+
         }
         curBal();
     }
     void curBal()
     {
+
         l.log(Level.INFO, ()->"Current Balance :"+balance);
     }
     void display()
     {
-        l.log(Level.INFO, ()->"Name:"+name+"\nAccount number "+accNo+ "\nBalance "+balance);
-    }
-    public static void main(String [] args)
-    {
-        Bankexeception kawin = new Bankexeception();
-
-        Scanner a = new Scanner(System.in);
-        boolean inc = true;
-        while(inc)
-        {
-            Logger l = Logger.getLogger("kawin");
-            l.info(" 1 Deposit \n 2 Withdrawl \n 3 Balance \n 4 Exit");
-            int choice = a.nextInt();
-            switch (choice) {
-                case 1 -> kawin.deposit();
-                case 2 -> kawin.withdrawl();
-                case 3 -> kawin.display();
-                case 4 -> inc = false;
-                default -> l.info("invalid choice");
-            }
-
-        }
+        l.log(Level.INFO  , ()->"Name:"+name+"\nAccount number "+accno+ "\nBalance "+balance);
     }
 }
